b34acf102876c0b85dd5e54ddb9471d94e9817c8
360Parminder <yesask8@gmail.com>
2025-06-12 09:47:24 +0530

    feat: integrate Vercel Analytics for improved tracking in the application

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastProvider } from './hooks/useToast'
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastProvider>
       <App />
     </ToastProvider>
+    <Analytics />
   </StrictMode>,
 )
